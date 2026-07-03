cf0da7bea1f162f88623972a9f3343e9f90701ca
SocialBureauWeb <web@socialbureau.in>
2026-07-03 10:15:05 +0530

    first commit

---
+++ Client/src/Pages/Ramadevi.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ProfilePortal from './ProfilePortal.jsx'
+
+export default function Ramadevi() {
+  // Use the central ProfilePortal to render the correspondent page by email
+  return <ProfilePortal identifier={'ramadevi@bigtv.com'} />
+}
